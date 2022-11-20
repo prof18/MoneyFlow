d5b2441d2fd5dbd65c6c84832cb2e845218463ad
Marco Gomiero <marco.gomiero.93@gmail.com>
2022-11-20 15:37:51 +0100

    Fix CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,6 @@ pluginManagement {
         mavenCentral()
     }
     dependencyResolutionManagement {
-        repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
         repositories {
             google()
             mavenCentral()
