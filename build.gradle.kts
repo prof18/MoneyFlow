db4c5582caf3fa47981ce3f28ce038a71cda8b1e
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-11-26 15:08:54 +0100

    Add android CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,9 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension.Companion.DEFAULT_SRC_DIR_JAVA
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension.Companion.DEFAULT_SRC_DIR_KOTLIN
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension.Companion.DEFAULT_TEST_SRC_DIR_JAVA
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension.Companion.DEFAULT_TEST_SRC_DIR_KOTLIN
-import java.io.ByteArrayOutputStream
 
 plugins {
-    alias(libs.plugins.versionsBenManes)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
@@ -17,12 +13,10 @@ plugins {
     alias(libs.plugins.sqldelight) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.triplet.play) apply false
 }
 
 val javaVersion by extra { JavaVersion.VERSION_21 }
-
-
-
 
 allprojects {
     apply {
---
+++ versioning.gradle.kts
@@ -0,0 +1,36 @@
+import java.util.Properties
+import java.text.SimpleDateFormat
+import java.util.Date
+
+val versionProps = Properties()
+val versionPropertiesFile = rootProject.file("version.properties")
+if (versionPropertiesFile.exists()) {
+    versionPropertiesFile.inputStream().use { versionProps.load(it) }
+} else {
+    throw GradleException("Root project version.properties not found! Please ensure it exists with MAJOR, MINOR, PATCH values.")
+}
+
+val appMajorVersion = versionProps.getProperty("MAJOR").toInt()
+val appMinorVersion = versionProps.getProperty("MINOR").toInt()
+val appPatchVersion = versionProps.getProperty("PATCH").toInt()
+
+fun getCurrentTimestamp(): String {
+    val sdf = SimpleDateFormat("yyyyMMddHHmm")
+    return sdf.format(Date())
+}
+
+fun appVersionCode(): Int {
+    val ciBuildNumber = System.getenv("GITHUB_RUN_NUMBER")
+    return if (ciBuildNumber != null) {
+        ciBuildNumber.toInt() + 1000
+    } else {
+        2 // Local build version code
+    }
+}
+
+fun appVersionName(): String {
+    return "${appMajorVersion}.${appMinorVersion}.${appPatchVersion}"
+}
+
+project.extra.set("appVersionCode", ::appVersionCode)
+project.extra.set("appVersionName", ::appVersionName)
